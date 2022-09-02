2ec83027cd0c690edffcc888290df8998b00bcf7
skyplaygame <112678396+skyplaygame@users.noreply.github.com>
2022-09-02 10:12:16 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When Global NFT marketplace?",
+    answer: "2022.3Q",
+    options: [
+      "2022.3Q",
+      "2023.3Q",
+      "2023.4Q",
+      "2025.3Q"
+    ]
+  },
+    {
+    numb: 2,
+    question: "On Which Network $SKP token based?",
+    answer: "Polygon",
+    options: [
+      "Polygon",
+      "BSC",
+      "ADA",
+      "Ethereum"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Total Supply of Skyplay?",
+    answer: "10,000,000,000 SKP",
+    options: [ 
+      "10,000,000,0 SKP",
+      "10,000,000,00 SKP ",
+      "10,000,000,000 SKP",
+      "10,000,000 SKP"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many holders does skyplay have?",
+    answer: "10387 addresses",
+    options: [ 
+      "19387 addresses",
+      "10387 addresses",
+      "12387 addresses",
+      "15387 addresses"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How much tokens reserved for business?",
+    answer: "4,500,000,000 SKP",
+    options: [
+      "500,000,000 SKP",
+      "200,000,000 SKP",
+      "400,000,000 SKP",
+      "3,500,000,000 SKP"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
